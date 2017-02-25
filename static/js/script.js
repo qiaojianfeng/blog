07fe0923c62fb7b88ae9c24ab533859597b6b69b
QiaoJianFeng <845882161@qq.com>
2017-02-25 11:05:38 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -42,14 +42,14 @@ function backToTop() {
 
 }
 
-function search(){
+/*function search(){
     (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
         (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
         e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
     })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
     _st('install','{{site.swiftype_searchId}}','2.0.0');
-}
+}*/
 
 
 
